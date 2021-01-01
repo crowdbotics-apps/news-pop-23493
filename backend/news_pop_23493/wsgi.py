8b8baa82e17b1d17193254ec7db318fd18686bdb
Crowdbotics <team@crowdbotics.com>
2021-01-01 05:05:59 +0000

    Update models

--- backend/news_pop_23493/wsgi.py
+++ backend/news_pop_23493/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news_pop_23493.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news_pop_23493.settings")
 
 application = get_wsgi_application()
